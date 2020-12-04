f34e4fd152c9da6915cf66da0c58d5aec2aeeea4
Judre <Reyesjudre@gmail.com>
2020-12-03 19:50:22 -0800

    3/4 completed

---
+++ index.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const app = express();
+const dblib = require("./dblib.js");
+
+const multer = require("multer");
+const upload = multer();
+
+// Add middleware to parse default urlencoded form
+app.use(express.urlencoded({ extended: false }));
+
+// Setup EJS
+app.set("view engine", "ejs");
+
+// Enable CORS (see https://enable-cors.org/server_expressjs.html)
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+
+// Application folders
+app.use(express.static("public"));
+
+// Start listener
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server started (http://localhost:3000/) !");
+});
+
+// Setup routes
+// app.get("/", (req, res) => {
+//     //res.send("Root resource - Up and running!")
+//     res.render("index");
+// });
+
+
+app.get("/", async (req, res) => {
+    // Omitted validation check
+    const totRecs = await dblib.getTotalRecords();
+    //Create an empty product object (To populate form with values)
+    const cars = {
+        carvin: "",
+        carmake: "",
+        carmodel: "",
+        carmileage: ""
+    };
+    res.render("index", {
+        type: "get",
+        totRecs: totRecs.totRecords,
+        car: cars
+    });
+});
+
+app.post("/", async (req, res) => {
+    // Omitted validation check
+    //  Can get this from the page rather than using another DB call.
+    //  Add it as a hidden form value.
+    const totRecs = await dblib.getTotalRecords();
+
+    dblib.findCar(req.body)
+        .then(result => {
+            res.render("index", {
+                type: "post",
+                totRecs: totRecs.totRecords,
+                result: result,
+                car: req.body
+            })
+        })
+        .catch(err => {
+            res.render("index", {
+                type: "post",
+                totRecs: totRecs.totRecords,
+                result: `Unexpected Error: ${err.message}`,
+                car: req.body
+            });
+        });
+});
